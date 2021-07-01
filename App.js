8c895677086bb748f25426eacafd2ce8cc019949
mingwencode <mingwen314@gmail.com>
2021-07-01 15:47:19 -0700

    added grid

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>What is it?</Text>
+
+      <View style={{backgroundColor: "yellow"}}>
+
+        <View style={{flexDirection: "row"}}>
+          <View style={styles.tile} />
+          <View style={styles.tile} />
+          <View style={styles.tile} />
+          <View style={styles.tile} />
+        </View>
+
+        <View style={{flexDirection: "row"}}>
+          <View style={styles.tile} />
+          <View style={styles.tile} />
+          <View style={styles.tile} />
+          <View style={styles.tile} />
+        </View>
+
+        <View style={{flexDirection: "row"}}>
+          <View style={styles.tile} />
+          <View style={styles.tile} />
+          <View style={styles.tile} />
+          <View style={styles.tile} />
+        </View>
+
+        <View style={{flexDirection: "row"}}>
+          <View style={styles.tile} />
+          <View style={styles.tile} />
+          <View style={styles.tile} />
+          <View style={styles.tile} />
+        </View>
+
+        <View style={{flexDirection: "row"}}>
+          <View style={styles.tile} />
+          <View style={styles.tile} />
+          <View style={styles.tile} />
+          <View style={styles.tile} />
+        </View>
+      </View>
+
+      <TouchableOpacity
+        onPress={()=>alert('Hello world')}
+        style ={styles.button}>
+        <Text style={styles.buttonText}>Start</Text>
+        </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +62,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: 'blue',
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: 'white',
+  },
+  tile: {
+    borderWidth: 1,
+    width: 100,
+    height: 100,
+  }
 });
